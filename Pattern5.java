9c488dd77ebcea4055b84814f8e27967bd97366d
snehachaudhari04 <98158739+snehachaudhari04@users.noreply.github.com>
2023-07-03 06:45:19 -0700

    Add files via upload
    1 2 3 4 5 
    1 2 3 4 
    1 2 3 
    1 2 
    1

---
+++ Pattern5.java
@@ -0,0 +1,42 @@
+
+class Pattern5
+{
+    void star(int n) {
+        for(int j=n;j>=1;j--){
+                for(int k=1;k<=j;k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+            
+        }
+        
+        
+    }
+ public static void main(String args[]){ 
+    Pattern5 p= new Pattern5();
+    p.star(5);
+   
+ }
+} 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
